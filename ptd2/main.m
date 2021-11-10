c6adf703da73d38361ba2b0350c870019639a5d5
ryan <chai_xb@163.com>
2021-11-11 01:00:26 +0800

    Initial Commit

---
+++ ptd2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ptd2
+//
+//  Created by ryan on 2021/11/11.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
